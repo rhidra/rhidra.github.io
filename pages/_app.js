1a1e49cf0b4d38b0293c8795433456a74168a56d
Rémy Hidra <remy.hidra@gmail.com>
2020-10-28 18:55:52 +0100

    Setup Chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/core';
+import Head from 'next/head';
+
+const theme = extendTheme({
+  fonts: {
+    body: "Open Sans, sans-serif",
+    heading: "Spartan, serif",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Rémy Hidra - Freelance Web Developer</title>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Spartan:wght@700&display=swap" rel="stylesheet"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp
