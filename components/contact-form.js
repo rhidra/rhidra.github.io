1cd1b1934832231ead9a9b37321564dda652adf5
Rémy Hidra <remy.hidra@gmail.com>
2020-10-30 18:37:46 +0100

    Add correct UX on UI for contact form

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,8 +1,16 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { FormControl, FormLabel, FormErrorMessage, Button, Input, LightMode, DarkMode, Textarea } from '@chakra-ui/core';
+import { FormControl, FormLabel, FormErrorMessage, Button, Input, LightMode, DarkMode, Textarea, useToast } from '@chakra-ui/core';
+import {AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay} from "@chakra-ui/core"
 import { RiSendPlaneFill } from 'react-icons/ri';
+import { FaLinkedin } from 'react-icons/fa';
+import {useRef, useState} from 'react';
 
 export default function ContactForm() {
+  const cancelRef = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false)
+  const toast = useToast();
+
   function validate(values) {
     const errors = {};
     if (!values.email) {
@@ -18,11 +26,24 @@ export default function ContactForm() {
     return errors;
   }
 
-  function submit(values, {setSubmitting}) {
-    console.log(values, setSubmitting);
+  async function submit(values, {setSubmitting}) {
+    const res = await fetch('/api/contact', {method: 'POST', body: JSON.stringify(values)});
+    setSubmitting(false);
+    if (!res.ok) {
+      setIsOpen(true);
+    } else {
+      toast({
+        title: "Request successfully sent !",
+        description: "I will answer you about your project very soon ! Thank you for contacting me !",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
   }
 
   return (
+    <>
     <Formik initialValues={{email: '', name: '', msg: ''}} validate={validate} onSubmit={submit}>
       {(props) => (
         <Form>
@@ -59,5 +80,29 @@ export default function ContactForm() {
         </Form>
       )}
     </Formik>
+
+    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Network error !
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              I am having trouble reaching my server :S ! Would you mind contacting me through another platform instead ?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button as="a" target="_blank" href="https://www.linkedin.com/in/rhidra/" leftIcon={<FaLinkedin/>} colorScheme="blue" ml={3}>
+                Contact on Linkedin
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 }
---
+++ pages/api/contact.js
@@ -0,0 +1,4 @@
+export default (req, res) => {
+  console.log(req.body);
+  res.status(200).json({});
+}
