cbfed34f9777437f45ce678e2290718410135071
Rémy Hidra <remy.hidra@gmail.com>
2020-10-30 15:57:43 +0100

    Add some Projects and fix the about me page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { Container, Flex, Heading, Box, Image, Center, List, ListItem, ListIcon, Text, HStack, Icon, Wrap, WrapItem, Tooltip, Button, DarkMode, Divider, Progress, SimpleGrid } from '@chakra-ui/core';
-import {FaNode, FaReact, FaAngular, FaDocker} from 'react-icons/fa';
-import {DiMongodb} from 'react-icons/di';
+import { Container, Flex, Heading, Box, Image, Center, Block, Text, Icon, Wrap, WrapItem, Tooltip, Button, DarkMode, Divider, SimpleGrid } from '@chakra-ui/core';
+import {FaNode, FaReact, FaAngular, FaDocker, FaPython, FaJs, FaJava} from 'react-icons/fa';
+import {DiIonic, DiMongodb} from 'react-icons/di';
 import {RiGitBranchFill} from 'react-icons/ri';
 import ProjectCard from '../components/project-card';
 
@@ -27,6 +27,10 @@ const logos = [{
 
 function Strong(props) {
   return <Text as="span" color="red.500" {...props}>{props.children}</Text>
+}
+
+function Paragraph(props) {
+  return <Text mb={3} style={{textIndent: 20}} p={[0, 2]} color="white" {...props}>{props.children}</Text>
 }
 
 export default function Home() {
@@ -60,12 +64,9 @@ export default function Home() {
             </Heading>
 
             <Text mt={2} mb={4} fontSize={['md', 'lg']}>
-              <Strong m={[2, 4]}>//</Strong>
-              French 
-              <Strong m={[2, 4]}>//</Strong>
-              Freelance
-              <Strong m={[2, 4]}>//</Strong>
-              Full-stack web developer
+              <Strong m={[2, 4]}>//</Strong> French 
+              <Strong m={[2, 4]}>//</Strong> Freelance
+              <Strong m={[2, 4]}>//</Strong> Full-stack web developer
               <Strong m={[2, 4]}>//</Strong>
             </Text>
 
@@ -87,54 +88,60 @@ export default function Home() {
           About me
         </Heading>
         
-        <Flex direction={['column', 'row']} textShadow="1px 1px 1px #222">
-          <Text color="white" flex={.5} p={[0, 4]}>
-            <Text mb={6} style={{textIndent: 20}}>
-              I am Rémy, a <Strong>fullstack web developer</Strong> and a part-time student pursuing a Masters degree in Telecommunications 
-              and Navigation Systems at <Strong>Shanghai Jiao Tong University</Strong> and <Strong>INSA Lyon</Strong>.
-            </Text>
-            
-            <Text style={{textIndent: 20}}>
-              Passionate about new technologies, I have self taught myself most skills required to build
-              complex infrastructures. With a good experience designing production systems, I am capable of
-              building a secure, efficient and responsive <Strong>Web and Mobile based solution</Strong>. 
-              I have experience with modern technologies
-              like <Strong>NodeJS</Strong>, <Strong>ReactJS</Strong>, <Strong>React-Native</Strong>, <Strong>MongoDB</Strong>, 
-              <Strong>Angular</Strong>, <Strong>Ionic</Strong>, <Strong>Python</Strong>, <Strong>Docker</Strong> and <Strong>Sass</Strong>.
-            </Text>
-          </Text>
+        <SimpleGrid columns={[1, 1, 2]}>
+          <Paragraph>
+            I am Rémy, a <Strong>fullstack web developer</Strong> and a part-time student pursuing a Masters degree in Telecommunications 
+            and Navigation Systems at <Strong>Shanghai Jiao Tong University</Strong> and <Strong>INSA Lyon</Strong>.
+          </Paragraph>
+          
+          <Paragraph>
+            Used to working in an <Strong>Agile environment</Strong>, I value including my client directly in the development process.
+            In order to make the most efficient software as possible, I always communicate with everyone in the team.
+          </Paragraph>
 
-          <Text flex={.5} p={[0, 4]}>
-            <Text color="white" mb={6} style={{textIndent: 20}}>
-              Used to working in an <Strong>Agile environment</Strong>, I value including my client directly in the development process.
-              In order to make the most efficient software as possible, I always communicate with everyone in the team.
-            </Text>
+          <Paragraph>
+            Passionate about new technologies, I have self taught myself most skills required to build
+            complex infrastructures. With a good experience designing production systems, I am capable of
+            building a secure, efficient and responsive <Strong>Web and Mobile based solution</Strong>. 
+            I have experience with modern technologies
+            like <Strong>NodeJS</Strong>, <Strong>ReactJS</Strong>, <Strong>React-Native</Strong>, <Strong>MongoDB</Strong>, 
+            <Strong>Angular</Strong>, <Strong>Ionic</Strong>, <Strong>Python</Strong>, <Strong>Docker</Strong> and <Strong>Sass</Strong>.
+          </Paragraph>
 
-            <Text color="white" style={{textIndent: 20}}>
-              Finally, my academic background gave me strong theoritical knowledge in <Strong>artificial intelligence</Strong>, <Strong>advanced
-              communication protocols</Strong> and <Strong>robot navigation</Strong>.
-              Not only do I appreciate learning new challenging topics, I believe some beauty can be found in creating elegant 
-              solutions to solve complex problems.
-            </Text>
-          </Text>
-        </Flex>
+
+          <Paragraph>
+            Finally, my academic background gave me strong theoritical knowledge in <Strong>artificial intelligence</Strong>, <Strong>advanced
+            communication protocols</Strong> and <Strong>robot navigation</Strong>.
+            Not only do I appreciate learning new challenging topics, I believe some beauty can be found in creating elegant 
+            solutions to solve complex problems.
+          </Paragraph>
+        </SimpleGrid>
 
         <Divider mb={12} mt={10}/>
 
-        <Heading color="red.500" textShadow="0px 3px 3px #111" fontSize={50} mb={3}>
+        <Heading color="red.500" textShadow="0px 3px 3px #111" fontSize={50} mb={[6, 3]}>
           Projects
         </Heading>
-        <Heading as="h3" color="gray.500" fontSize={20} mb={6} fontStyle="oblique 10deg">
+        <Heading display={['none', 'block']} as="h3" color="gray.500" fontSize={20} mb={6} fontStyle="oblique 10deg">
           Hover to see more !
         </Heading>
 
         <SimpleGrid spacing={4} minChildWidth="400px">
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-          <ProjectCard/>
+          <ProjectCard title="CRM Mobile App"
+                      img="img/kerlead/cover.png"
+                      icons={[FaAngular, DiIonic, FaJava, DiMongodb]}
+                      link="https://kerlead.fr">
+          </ProjectCard>
+
+          <ProjectCard title="Video collective management software"
+                      img="img/mouette/cover.png"
+                      icons={[FaPython, FaPython, FaJs]}
+                      link="https://medialamouette.fr">
+            A small platform for video gear management, for a small collective.
+            The app is used to register and rent video gear and manage projects.
+            It also features automatic PDF generation of invoices and payments. 
+            To coordinate the system through users, we implemented email and discord notifications.
+          </ProjectCard>
         </SimpleGrid>
 
         <Divider mb={12} mt={10}/>
