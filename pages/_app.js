d014a997d7f1928541ab7b73bea6614553eda965
Rémy Hidra <remy.hidra@gmail.com>
2021-01-10 23:32:49 +0100

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -54,7 +54,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Rémy Hidra // Freelance Web Developer</title>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Spartan:wght@700&display=swap" rel="stylesheet"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
