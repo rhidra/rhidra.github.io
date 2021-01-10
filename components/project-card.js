e4bf3eddf40d6b1b39d181ae296ee72ad9e62e90
Rémy Hidra <remy.hidra@gmail.com>
2021-01-10 19:27:59 +0100

    Cleanup cards

--- components/project-card.js
+++ components/project-card.js
@@ -1,6 +1,4 @@
-import { AspectRatio, Box, Image, Heading, Text, WrapItem, Wrap, Icon, Button, HStack, Flex, Spacer, useDisclosure, DarkMode, useTheme } from "@chakra-ui/core";
-import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from "@chakra-ui/core"
-import { FaGithub, FaEye } from 'react-icons/fa';
+import { Box, Image, Heading, Text, WrapItem, Wrap, Icon, Button, HStack, Flex, Spacer, useDisclosure, DarkMode, useTheme } from "@chakra-ui/core";
 import {useRouter} from 'next/router';
 
 export default function ProjectCard({img, children, icons, title, type}) {
@@ -35,72 +33,8 @@ export default function ProjectCard({img, children, icons, title, type}) {
       </Text>
 
       <Flex flexDir="row" justifyContent="center">
-        {icons.map((icon, key) => <Icon as={icon} color="black" boxSize="30px" mr={[2, 4]} ml={[2, 4]}/>)}
+        {icons.map((icon, key) => <Icon as={icon} color="black" boxSize="30px" mr={[1, 4]} ml={[1, 4]}/>)}
       </Flex>
     </Flex>
   );
-      
-      
-      {/*
-      <Box display={['none', 'block']} className="slide" bg="red.600" p={4} overflow="hidden">
-        <Flex direction="column" h="100%" pb={3} pt={3}>
-          <Wrap direction="row" justify="center" color="white" mb={4}>
-            {props.icons.map((icon, key) => (
-              <WrapItem key={key}>
-                <Icon as={icon} boxSize="40px"/>
-              </WrapItem>
-            ))}
-          </Wrap>
-
-          </Text>
-
-          <Spacer/>
-
-          <HStack justify="center" spacing={4} mt={3}>
-            {props.source &&
-              <Button as="a" target="_blank" href={props.source} leftIcon={<FaGithub/>}>
-                Source
-              </Button>
-            }
-            
-            {props.link && 
-              <Button as="a" target="_blank" href={props.link} leftIcon={<FaEye/>}>
-                Link
-              </Button>
-            }
-          </HStack>
-        </Flex>
-      </Box>
-
-      <DarkMode>
-      <Modal display={['block', 'none']} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader color="white" fontFamily="Spartan">{props.title}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody color="white">
-            {props.children}
-          </ModalBody>
-
-          <ModalFooter>
-            <Button variant="ghost" mr={3} onClick={onClose}>
-              Close
-            </Button>
-
-            {props.source &&
-              <Button colorScheme="red" as="a" target="_blank" mr={3} href={props.source} leftIcon={<FaGithub/>}>
-                Source
-              </Button>
-            }
-            
-            {props.link && 
-              <Button colorScheme="red" as="a" target="_blank" href={props.link} leftIcon={<FaEye/>}>
-                Link
-              </Button>
-            }
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      </DarkMode>
-      */}
 }
