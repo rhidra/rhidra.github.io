a763f6d81b0e8c37d9ee22e19d5cc70749506ab3
Rémy Hidra <remy.hidra@gmail.com>
2021-01-10 21:50:49 +0100

    Add a card animation to display text

--- components/project-card.js
+++ components/project-card.js
@@ -1,40 +1,55 @@
-import { Box, Image, Heading, Text, WrapItem, Wrap, Icon, Button, HStack, Flex, Spacer, useDisclosure, DarkMode, useTheme } from "@chakra-ui/core";
+import { Box, Image, Heading, Text, WrapItem, Wrap, Icon, Button, HStack, Flex, Spacer, useDisclosure, DarkMode, useTheme, Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@chakra-ui/core";
 import {useRouter} from 'next/router';
+import {FaCaretDown, FaCaretUp} from 'react-icons/fa';
 
 export default function ProjectCard({img, children, icons, title, type}) {
   const theme = useTheme();
   const router = useRouter();
 
   return (
-    <Flex 
-      className="card" mb={10}
-      borderRadius="lg" bg="gray.50"
-      boxShadow="base" maxW="4xl"
-      flexDir="column"
-      pb={6}
-      // onClick={() => router.push('/page')}
-    >
-      <Flex flexDir="row" alignItems="center">
-        <Image src={img} w={[20, 40]} h={[20, 40]} m={[4, 12]} borderRadius="full" boxShadow="md"/>
+    <Accordion allowMultiple mb={10}>
+      <AccordionItem border="none">
+        {({isExpanded}) => (
+          <AccordionButton boxShadow="none !important">
+            <Flex 
+              className="card"
+              borderRadius="lg" bg="gray.50"
+              boxShadow="base" maxW="5xl"
+              flexDir="column"
+              pb={6}
+              // onClick={() => router.push('/page')}
+            >
+              <Flex flexDir="row" alignItems="center">
+                <Image transition=".2s all" src={img} w={[20, isExpanded ? 40 : 20]} h={[20, isExpanded ? 40 : 20]} m={[4, 12]} borderRadius="full" boxShadow="md"/>
 
-        <Box pt={6} pr={12}>
-          <Heading as="h3" fontSize="xs" textTransform="uppercase" letterSpacing={1}>{type}</Heading>
+                <Box pt={6} pr={6}>
+                  <Heading as="h3" fontSize="xs" textTransform="uppercase" letterSpacing={1} textAlign="left">{type}</Heading>
 
-          <Heading as="h4" fontSize="md" textTransform="uppercase" lineHeight={5} mb={4}>{title}</Heading>
+                  <Heading as="h4" fontSize="md" textTransform="uppercase" lineHeight={5} mb={4} textAlign="left">{title}</Heading>
 
-          <Text fontSize="md" color="gray.500" mb={10} maxW="35em" display={['none', 'block']}>
-            {children}
-          </Text>
-        </Box>
-      </Flex>
+                  <AccordionPanel p={0}>
+                    <Text fontSize="md" color="gray.500" mb={10} maxW="35em" textAlign="left" display={['none', 'block']}>
+                      {children}
+                    </Text>
+                  </AccordionPanel>
+                </Box>
 
-      <Text fontSize="md" color="gray.500" mb={10} pt={4} pl={4} pr={4} maxW="35em" display={['block', 'none']}>
-        {children}
-      </Text>
+                <Icon display={['none', 'block']} as={isExpanded ? FaCaretUp : FaCaretDown} boxSize="40px" mr={10}/>
+              </Flex>
 
-      <Flex flexDir="row" justifyContent="center">
-        {icons.map((icon, key) => <Icon as={icon} color="black" boxSize="30px" mr={[1, 4]} ml={[1, 4]}/>)}
-      </Flex>
-    </Flex>
+              <AccordionPanel>
+                <Text fontSize="md" color="gray.500" mb={10} pt={4} pl={4} pr={4} maxW="35em" textAlign="left" display={['block', 'none']}>
+                  {children}
+                </Text>
+
+                <Flex flexDir="row" justifyContent="center">
+                  {icons.map((icon, key) => <Icon as={icon} color="black" boxSize="30px" mr={[1, 4]} ml={[1, 4]}/>)}
+                </Flex>
+              </AccordionPanel>
+            </Flex>
+          </AccordionButton>
+        )}
+      </AccordionItem>
+    </Accordion>
   );
 }
