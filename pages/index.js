849b920f7c5ff77ab6590d2b4161a1eaed86aafa
Rémy Hidra <remy.hidra@gmail.com>
2021-01-10 23:33:32 +0100

    Invert about me and projects sections

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Container, Flex, Heading, Box, Image, Center, Text, Icon, Wrap, WrapIte
 import {FaNode, FaReact, FaAngular, FaDocker, FaPython, FaJs, FaJava, FaBootstrap, FaGithub, FaEnvelope, FaLinkedin} from 'react-icons/fa';
 import {DiIonic, DiMongodb, DiDjango, DiNginx} from 'react-icons/di';
 import {RiGitBranchFill} from 'react-icons/ri';
+import {SiNextDotJs} from 'react-icons/si';
 import ProjectCard from '../components/project-card';
 import ContactForm from '../components/contact-form';
 import config from '../config';
@@ -118,38 +119,6 @@ export default function Home() {
     <Container maxWidth="100%" p="0" bg="purple.900">
       <Box w="100%" minHeight="150vh" pt={[5, 100]} pl={[5, 150]} pr={[5, 150]}>
 
-        <Heading color="red.500" textShadow="0px 3px 3px #111" fontSize={50} mb={8}>
-          About me
-        </Heading>
-        
-        <SimpleGrid columns={[1, 1, 2]} mb={12}>
-          <Paragraph>
-            I am Rémy, a <Strong>fullstack web developer</Strong> and a part-time student pursuing a Masters degree in Telecommunications 
-            and Navigation Systems at <Strong>Shanghai Jiao Tong University</Strong> and <Strong>INSA Lyon</Strong>.
-          </Paragraph>
-          
-          <Paragraph>
-            Used to working in an <Strong>Agile environment</Strong>, I value including my client directly in the development process.
-            In order to make the most efficient software as possible, I always communicate with everyone in the team.
-          </Paragraph>
-
-          <Paragraph>
-            Passionate about new technologies, I have self taught myself most skills required to build
-            complex infrastructures. With a good experience designing production systems, I am capable of
-            building a secure, efficient and responsive <Strong>Web and Mobile based solution</Strong>. 
-            I have experience with modern technologies
-            like <Strong>NodeJS</Strong>, <Strong>ReactJS</Strong>, <Strong>React-Native</Strong>, <Strong>MongoDB</Strong>, 
-            <Strong>Angular</Strong>, <Strong>Ionic</Strong>, <Strong>Python</Strong>, <Strong>Docker</Strong> and <Strong>Sass</Strong>.
-          </Paragraph>
-
-          <Paragraph>
-            Finally, my academic background gave me strong theoritical knowledge in <Strong>artificial intelligence</Strong>, <Strong>advanced
-            communication protocols</Strong> and <Strong>robot navigation</Strong>.
-            Not only do I appreciate learning new challenging topics, I believe some beauty can be found in creating elegant 
-            solutions to solve complex problems.
-          </Paragraph>
-        </SimpleGrid>
-
         <Heading color="red.500" textShadow="0px 3px 3px #111" fontSize={50} mb={12}>
           Projects
         </Heading>
@@ -165,6 +134,18 @@ export default function Home() {
             users video upload and processing and a scalable infrastructure. As the only dev, 
             I had to build an app easy to iterate on and maintain, without any third party
             technologies potentially blocked in China.
+          </ProjectCard>
+
+          <ProjectCard title="Portfolio"
+                      type="Frontend"
+                      img="img/portfolio/icon.png"
+                      icons={[FaReact, SiNextDotJs]}
+                      source="https://github.com/rhidra/rhidra.github.io"
+                      link="https://rhidra.github.io">
+            A simple portfolio made with React, NextJS and Chakra UI.
+            The goal was to design a good looking app from scratch, without any template or
+            strong external reference. The interface should also be SEO optimized and quickly
+            readable by a potential client.
           </ProjectCard>
 
           <ProjectCard title="Kerlead: CRM Mobile App"
@@ -202,9 +183,37 @@ export default function Home() {
           </ProjectCard>
         </Flex>
 
-        <Flex justify="center" id="contact">
-          <Divider opacity="1" w="100px" mb={12} mt={12} borderBottomWidth="3px" borderTopWidth="3px" borderRadius="20px" borderColor="red.600"/>
-        </Flex>
+        <Heading color="red.500" textShadow="0px 3px 3px #111" fontSize={50} mt={6} mb={8}>
+          About me
+        </Heading>
+        
+        <SimpleGrid columns={[1, 1, 2]} mb={12}>
+          <Paragraph>
+            I am Rémy, a <Strong>fullstack web developer</Strong> and a part-time student pursuing a Masters degree in Telecommunications 
+            and Navigation Systems at <Strong>Shanghai Jiao Tong University</Strong> and <Strong>INSA Lyon</Strong>.
+          </Paragraph>
+          
+          <Paragraph>
+            Used to working in an <Strong>Agile environment</Strong>, I value including my client directly in the development process.
+            In order to make the most efficient software as possible, I always communicate with everyone in the team.
+          </Paragraph>
+
+          <Paragraph>
+            Passionate about new technologies, I have self taught myself most skills required to build
+            complex infrastructures. With a good experience designing production systems, I am capable of
+            building a secure, efficient and responsive <Strong>Web and Mobile based solution</Strong>. 
+            I have experience with modern technologies
+            like <Strong>NodeJS</Strong>, <Strong>ReactJS</Strong>, <Strong>React-Native</Strong>, <Strong>MongoDB</Strong>, 
+            <Strong>Angular</Strong>, <Strong>Ionic</Strong>, <Strong>Python</Strong>, <Strong>Docker</Strong> and <Strong>Sass</Strong>.
+          </Paragraph>
+
+          <Paragraph>
+            Finally, my academic background gave me strong theoritical knowledge in <Strong>artificial intelligence</Strong>, <Strong>advanced
+            communication protocols</Strong> and <Strong>robot navigation</Strong>.
+            Not only do I appreciate learning new challenging topics, I believe some beauty can be found in creating elegant 
+            solutions to solve complex problems.
+          </Paragraph>
+        </SimpleGrid>
 
         <Heading color="red.500" textShadow="0px 3px 3px #111" fontSize={50} mb={12}>
           Contact
@@ -236,5 +245,5 @@ export default function Home() {
       </Box>
     </Container>
     </>
-  )
+  );
 }
