8f41e3f25712e40a920fd2ff80b4bca88af0de7c
Rémy Hidra <remy.hidra@gmail.com>
2021-01-13 20:36:44 +0100

    Add google search console verification

--- pages/_app.js
+++ pages/_app.js
@@ -58,6 +58,8 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
+
+        <meta name="google-site-verification" content="zhO_vjy3H84BJz-sXTCFRv7e2gf0y1GX5ojB0KA0PYw" />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
