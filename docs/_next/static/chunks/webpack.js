89f893b7a3a779cb8aa24ffcfd7d57bb66309cf4
Rémy Hidra <remy.hidra@gmail.com>
2021-01-23 18:44:40 +0100

    auto: regen site

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5c79cb2fcea4335f86bc";
+/******/ 	var hotCurrentHash = "de55fd5ad7b5566c4282";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
