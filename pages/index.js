18a9957f618ac946b35eac70e6647dc4785fe539
Rémy Hidra <remy.hidra@gmail.com>
2021-07-06 15:51:46 +0200

    Add the fluid simulation project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { Container, Flex, Heading, Box, Image, Center, Text, Icon, Wrap, WrapItem, Tooltip, Button, DarkMode, Divider, SimpleGrid, ButtonGroup } from '@chakra-ui/core';
 import {FaNode, FaReact, FaAngular, FaDocker, FaPython, FaJs, FaJava, FaBootstrap, FaGithub, FaEnvelope, FaLinkedin, FaAndroid, FaApple} from 'react-icons/fa';
-import {DiIonic, DiMongodb, DiDjango, DiNginx} from 'react-icons/di';
+import {DiIonic, DiMongodb, DiDjango, DiNginx, DiJavascript1} from 'react-icons/di';
 import {RiGitBranchFill, RiFlutterFill} from 'react-icons/ri';
-import {SiNextDotJs, SiFirebase, SiRedux, SiSass, SiHeroku, SiGooglecloud, SiMaterialUi} from 'react-icons/si';
+import {SiNextDotJs, SiFirebase, SiRedux, SiSass, SiHeroku, SiGooglecloud, SiMaterialUi, SiJavascript, SiWebgl, SiWebpack, SiTypescript} from 'react-icons/si';
 import ProjectCard from '../components/project-card';
 import ContactForm from '../components/contact-form';
 import config from '../config';
@@ -173,6 +173,19 @@ export default function Home() {
             Using Ionic 4 and Angular 2, the UI is efficient, inspired by the 
             current CRM web app, but adapted to a mobile environment. 
             The app was be tested on Android and iOS, on smartphone and tablet. 
+          </ProjectCard>
+
+          <ProjectCard title="Fluid Simulation Experiment"
+                      img="img/fluid/icon.png"
+                      type="Frontend"
+                      icons={[DiJavascript1, SiTypescript, SiWebgl, SiWebpack]}
+                      source="https://github.com/rhidra/fluid-simulation"
+                      link="https://rhidra.github.io/fluid-simulation">
+            A fun experiment between fluid mechanics and generative art. To make the 
+            program run in real-time at full resolution, I had to make all the computations
+            run on the client GPU, using the native WebGL API. I implemented the algorithm
+            on sequential shaders operations. To make the app as light as possible,
+            only vanilla JS was used, compiled from Typescript using a small Webpack configuration.
           </ProjectCard>
 
           <ProjectCard title="Real-time Chat Mobile app"
