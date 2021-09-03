1e798934479b49f345a3ea3fd949148b723b3545
Rémy Hidra <remy.hidra@gmail.com>
2021-09-03 16:44:22 +0200

    Fix navbar contrast

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,15 +6,18 @@ export default function Navbar() {
   return (
     <DarkMode>
       <Flex display={['none', 'flex']} pos="fixed" zIndex="10" w="100%" h="85px" direction="row-reverse" p={4} 
-            background="linear-gradient(180deg, rgba(44, 44, 45, 0.37) 0%, rgba(255,255,255,0) 100%)">
+            background="linear-gradient(180deg, rgba(0, 0, 0, 0.7) 0%, rgba(0,0,0,0) 100%)">
         <Button variant="ghost" mr={3} as="a" href="#contact">
           Contact
+        </Button>
+        <Button variant="ghost" mr={3} as="a" href="#about">
+          About me
         </Button>
         <Button variant="ghost" mr={3} as="a" href="#projects">
           Projects
         </Button>
-        <Button variant="ghost" mr={3} as="a" href="#about">
-          About me
+        <Button variant="ghost" mr={3} as="a" href="#skills">
+          Tech Skills
         </Button>
 
         <Flex size="lg" mr={6}>
