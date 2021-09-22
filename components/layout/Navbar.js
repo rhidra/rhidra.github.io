8e8afaa4ff58923ae75cfa69209a187162ca5afa
Rémy Hidra <remy.hidra@gmail.com>
2021-09-22 23:29:38 +0200

    Improve navbar on scrolling

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,12 +1,36 @@
 import { Button, ButtonGroup, DarkMode, Flex, Icon } from "@chakra-ui/core";
+import { useEffect, useState } from "react";
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 import config from "../../config";
 
 export default function Navbar() {
+  const [showNav, setShowNav] = useState(true);
+
+  useEffect(() => {
+    let lastScrollTop;
+    const listener = () => {
+      // ref: https://dev.to/areeburrub/hide-navbar-as-scroll-down-in-less-than-10-lines-of-javascript-1i00
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop) {
+        // Scroll up
+        setShowNav(false);
+      } else {
+        // Scroll down
+        setShowNav(true);
+      }
+      lastScrollTop = scrollTop;
+    };
+
+    window.addEventListener('scroll', listener);
+
+    return () => window.removeEventListener('scroll', listener);
+  }, []);
+
   return (
     <DarkMode>
-      <Flex display={['none', 'flex']} pos="fixed" zIndex="20" w="100%" h="85px" direction="row-reverse" p={4} 
-            background="linear-gradient(180deg, rgba(0, 0, 0, 0.7) 0%, rgba(0,0,0,0) 100%)">
+      <Flex display={['none', 'flex']} alignItems="center" pos="fixed" zIndex="20" w="100%" h="70px" direction="row-reverse" px={4} 
+            background="linear-gradient(180deg, rgba(0, 0, 0, 0.7) 0%, rgba(0,0,0,0) 100%)"
+            transform={showNav ? 'translateY(0%)' : 'translateY(-100%)'} transition="transform linear 200ms">
         <Button variant="ghost" mr={3} as="a" href="#contact">
           Contact
         </Button>
