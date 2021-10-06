39ff23990c502b861661239c058bcb9803b7a305
Rémy Hidra <remy.hidra@gmail.com>
2021-10-06 18:12:09 +0200

    Fix JSX

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,9 @@ function MyApp({ Component, pageProps }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-1GLSHJY89E"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag() {'{'}
+            dataLayer.push(arguments);
+          {'}'}
           gtag('js', new Date());
 
           gtag('config', 'G-1GLSHJY89E');
